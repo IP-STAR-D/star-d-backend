e56f5d976c99a5383fbba9b6e4b0bde9de1d2ae8
Bîrsan Dorin-Alexandru <alexodrino@gmail.com>
2024-11-27 14:17:02 +0200

    Add cors

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
+const corsOptions = {
+  origin: ["http://localhost:4200", "https://star-d-frontend.netlify.app"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -32,7 +44,6 @@ require("./app/routes/degree.routes")(app); // routing for degrees
 require("./app/routes/student.routes")(app); // routing for students
 require("./app/routes/appointment.routes")(app); // routing for appointment
 require("./app/routes/auth.routes")(app); // routing for auth
-
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
